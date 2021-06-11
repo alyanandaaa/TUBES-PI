0cb99e7f0d37d61e813613568481a9b1bee62ebb
Alya Ananda <72023572+alyanandaaa@users.noreply.github.com>
2021-06-11 17:49:15 +0700

    itlg.sql

---
+++ itlg.sql
@@ -0,0 +1,490 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2021 at 05:58 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simaset`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asets`
+--
+
+CREATE TABLE `asets` (
+  `id_aset` varchar(128) NOT NULL,
+  `kode_aset` varchar(128) DEFAULT NULL,
+  `id_barang` int(11) DEFAULT NULL,
+  `id_lokasi` int(11) DEFAULT NULL,
+  `volume` int(11) DEFAULT NULL,
+  `satuan` varchar(50) DEFAULT NULL,
+  `harga` double DEFAULT NULL,
+  `total_harga` double DEFAULT NULL,
+  `kondisi` varchar(128) DEFAULT 'Baik',
+  `status_aset` varchar(50) DEFAULT NULL,
+  `umur_ekonomis` int(11) DEFAULT NULL,
+  `jenis_bantuan` varchar(128) DEFAULT NULL,
+  `jenis_aset` varchar(128) DEFAULT 'Berwujud',
+  `qr_code` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `asets`
+--
+
+INSERT INTO `asets` (`id_aset`, `kode_aset`, `id_barang`, `id_lokasi`, `volume`, `satuan`, `harga`, `total_harga`, `kondisi`, `status_aset`, `umur_ekonomis`, `jenis_bantuan`, `jenis_aset`, `qr_code`) VALUES
+('cf968cd16d264a07a82e843a72f07dab', '0001/TIK/2021', 6, 4, 10, 'Buah', 94000, 940000, 'Baru', 'Aktif', 5, 'Beli', 'Berwujud', 'af0a2ee159c441f784121ee5d04a74d0.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `merek` varchar(128) NOT NULL,
+  `tahun_perolehan` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `id_kategori`, `nama_barang`, `merek`, `tahun_perolehan`) VALUES
+(5, 4, 'Panasonic Lampu Bohlam LED 5 Watt', '', 2021),
+(6, 5, 'Mouse Logitech ', '', 2021);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_aset`
+--
+
+CREATE TABLE `data_aset` (
+  `id_aset` int(11) NOT NULL,
+  `nama_aset` varchar(128) DEFAULT NULL,
+  `harga` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_aset`
+--
+
+INSERT INTO `data_aset` (`id_aset`, `nama_aset`, `harga`) VALUES
+(1, 'Full Set Komputer Core i5 Lcd 19inc Acer', 3499000),
+(2, 'Full Set Komputer Core i5 Lcd 19inc Asus', 4000000),
+(3, 'Full Set Komputer Core i5 Lcd 19inc Lenovo', 3000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_barang`
+--
+
+CREATE TABLE `kategori_barang` (
+  `id_kategori` int(11) NOT NULL,
+  `kode_kategori` varchar(128) DEFAULT NULL,
+  `nama_kategori` varchar(128) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori_barang`
+--
+
+INSERT INTO `kategori_barang` (`id_kategori`, `kode_kategori`, `nama_kategori`, `updated_at`) VALUES
+(4, 'ELK', 'Lampu', '2021-06-02 22:34:10'),
+(5, 'TIK', 'Mouse ', '2021-06-02 22:34:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keputusan_pengadaan`
+--
+
+CREATE TABLE `keputusan_pengadaan` (
+  `id_nilai` int(11) NOT NULL,
+  `id_aset` int(11) DEFAULT NULL,
+  `id_spesifikasi` int(11) DEFAULT NULL,
+  `id_kualitas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `keputusan_pengadaan`
+--
+
+INSERT INTO `keputusan_pengadaan` (`id_nilai`, `id_aset`, `id_spesifikasi`, `id_kualitas`) VALUES
+(1, 1, 1, 2),
+(2, 2, 2, 1),
+(3, 3, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria_kualitas`
+--
+
+CREATE TABLE `kriteria_kualitas` (
+  `id_kualitas` int(11) NOT NULL,
+  `keterangan` varchar(128) DEFAULT NULL,
+  `nilai` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kriteria_kualitas`
+--
+
+INSERT INTO `kriteria_kualitas` (`id_kualitas`, `keterangan`, `nilai`) VALUES
+(1, 'Sangat Baik', 0.5),
+(2, 'Baik', 0.4),
+(3, 'Cukup', 0.3),
+(4, 'Jelek', 0.2),
+(5, 'Sangat Jelek', 0.1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria_spesifikasi`
+--
+
+CREATE TABLE `kriteria_spesifikasi` (
+  `id_spesifikasi` int(11) NOT NULL,
+  `keterangan` varchar(128) DEFAULT NULL,
+  `nilai` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kriteria_spesifikasi`
+--
+
+INSERT INTO `kriteria_spesifikasi` (`id_spesifikasi`, `keterangan`, `nilai`) VALUES
+(1, 'Sangat Baik', 0.5),
+(2, 'Baik', 0.4),
+(3, 'Cukup', 0.3),
+(4, 'Jelek', 0.2),
+(5, 'Sangat Jelek', 0.1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi_aset`
+--
+
+CREATE TABLE `lokasi_aset` (
+  `id_lokasi` int(11) NOT NULL,
+  `nama_lokasi` varchar(128) NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lokasi_aset`
+--
+
+INSERT INTO `lokasi_aset` (`id_lokasi`, `nama_lokasi`, `updated_at`) VALUES
+(4, 'Multimedia', '2021-06-02 22:40:43'),
+(5, 'Jaringan', '2021-06-02 22:41:30'),
+(6, 'Database', '2021-06-02 22:40:52'),
+(7, 'Pemrograman', '2021-06-02 22:42:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monitoring_aset`
+--
+
+CREATE TABLE `monitoring_aset` (
+  `id_monitoring` int(11) NOT NULL,
+  `id_aset` varchar(128) DEFAULT NULL,
+  `kerusakan` text,
+  `akibat` text,
+  `faktor` text,
+  `monitoring` text,
+  `pemeliharaan` text,
+  `jml_rusak` varchar(128) DEFAULT NULL,
+  `foto` varchar(128) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengadaan`
+--
+
+CREATE TABLE `pengadaan` (
+  `id_pengadaan` int(11) NOT NULL,
+  `id_lokasi` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `nama_aset` varchar(128) DEFAULT NULL,
+  `volume` int(11) DEFAULT NULL,
+  `satuan` varchar(128) DEFAULT NULL,
+  `harga_satuan` double DEFAULT NULL,
+  `tahun_pengadaan` varchar(4) DEFAULT NULL,
+  `status` enum('0','1','2') DEFAULT '0',
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penghapusan`
+--
+
+CREATE TABLE `penghapusan` (
+  `id_penghapusan` int(11) NOT NULL,
+  `id_aset` varchar(128) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `faktor` text,
+  `tgl_penghapusan` date DEFAULT NULL,
+  `status` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(6) NOT NULL,
+  `nama_user` varchar(125) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `jabatan` varchar(128) NOT NULL,
+  `role` enum('1','2','3') DEFAULT NULL,
+  `foto` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama_user`, `username`, `password`, `jabatan`, `role`, `foto`) VALUES
+(11, 'Astrid', 'astrid', 'afb91ef692fd08c445e8cb1bab2ccf9c', 'Petugas', '2', NULL),
+(13, 'Alya ananda', 'alyaa', '21232f297a57a5a743894a0e4a801fc3', 'Admin', '1', NULL),
+(14, 'Nabila Rizka', 'nabila', 'e0c4bde30c959d22007497388ca42a4e', 'Kepala Bidang Inventaris', '3', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `asets`
+--
+ALTER TABLE `asets`
+  ADD PRIMARY KEY (`id_aset`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_lokasi` (`id_lokasi`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_jenis` (`id_kategori`);
+
+--
+-- Indexes for table `data_aset`
+--
+ALTER TABLE `data_aset`
+  ADD PRIMARY KEY (`id_aset`);
+
+--
+-- Indexes for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `keputusan_pengadaan`
+--
+ALTER TABLE `keputusan_pengadaan`
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `id_spesifikasi` (`id_spesifikasi`),
+  ADD KEY `id_kualitas` (`id_kualitas`),
+  ADD KEY `id_aset` (`id_aset`);
+
+--
+-- Indexes for table `kriteria_kualitas`
+--
+ALTER TABLE `kriteria_kualitas`
+  ADD PRIMARY KEY (`id_kualitas`);
+
+--
+-- Indexes for table `kriteria_spesifikasi`
+--
+ALTER TABLE `kriteria_spesifikasi`
+  ADD PRIMARY KEY (`id_spesifikasi`);
+
+--
+-- Indexes for table `lokasi_aset`
+--
+ALTER TABLE `lokasi_aset`
+  ADD PRIMARY KEY (`id_lokasi`);
+
+--
+-- Indexes for table `monitoring_aset`
+--
+ALTER TABLE `monitoring_aset`
+  ADD PRIMARY KEY (`id_monitoring`),
+  ADD KEY `id_aset` (`id_aset`);
+
+--
+-- Indexes for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  ADD PRIMARY KEY (`id_pengadaan`),
+  ADD KEY `id_lokasi` (`id_lokasi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `penghapusan`
+--
+ALTER TABLE `penghapusan`
+  ADD PRIMARY KEY (`id_penghapusan`),
+  ADD KEY `id_aset` (`id_aset`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `data_aset`
+--
+ALTER TABLE `data_aset`
+  MODIFY `id_aset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `keputusan_pengadaan`
+--
+ALTER TABLE `keputusan_pengadaan`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kriteria_kualitas`
+--
+ALTER TABLE `kriteria_kualitas`
+  MODIFY `id_kualitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `kriteria_spesifikasi`
+--
+ALTER TABLE `kriteria_spesifikasi`
+  MODIFY `id_spesifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `lokasi_aset`
+--
+ALTER TABLE `lokasi_aset`
+  MODIFY `id_lokasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `monitoring_aset`
+--
+ALTER TABLE `monitoring_aset`
+  MODIFY `id_monitoring` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  MODIFY `id_pengadaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `penghapusan`
+--
+ALTER TABLE `penghapusan`
+  MODIFY `id_penghapusan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `asets`
+--
+ALTER TABLE `asets`
+  ADD CONSTRAINT `asets_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `asets_ibfk_2` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi_aset` (`id_lokasi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_barang` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `keputusan_pengadaan`
+--
+ALTER TABLE `keputusan_pengadaan`
+  ADD CONSTRAINT `keputusan_pengadaan_ibfk_1` FOREIGN KEY (`id_spesifikasi`) REFERENCES `kriteria_spesifikasi` (`id_spesifikasi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `keputusan_pengadaan_ibfk_2` FOREIGN KEY (`id_kualitas`) REFERENCES `kriteria_kualitas` (`id_kualitas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `keputusan_pengadaan_ibfk_3` FOREIGN KEY (`id_aset`) REFERENCES `data_aset` (`id_aset`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `monitoring_aset`
+--
+ALTER TABLE `monitoring_aset`
+  ADD CONSTRAINT `monitoring_aset_ibfk_1` FOREIGN KEY (`id_aset`) REFERENCES `asets` (`id_aset`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  ADD CONSTRAINT `pengadaan_ibfk_1` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi_aset` (`id_lokasi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pengadaan_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `penghapusan`
+--
+ALTER TABLE `penghapusan`
+  ADD CONSTRAINT `penghapusan_ibfk_1` FOREIGN KEY (`id_aset`) REFERENCES `asets` (`id_aset`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
